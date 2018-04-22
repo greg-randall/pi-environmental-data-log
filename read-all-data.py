f0b99bbef495e621dfb20db30232f5f4d99d0e8b
Greg Randall <gregrr@gmail.com>
2018-04-22 15:52:58 -0500

    added headers and timing

--- read-all-data.py
+++ read-all-data.py
@@ -1,21 +1,31 @@
 import tsl2561read
 import bme680read
 import time
+import os.path
+
+#logging period in seconds:
+loggingperiod = 10
 
 
-print "temperature F, pressure inch inHg, %RH, visible light lux, infrared light lux"
+headers = "time & date, temperature F, pressure inch inHg, %RH, visible light lux, infrared light lux\n"
 
-outputfile = open("log.csv","w")
+if os.path.exists("log.csv"):
+  outputfile = open("log.csv","a")
+else:
+  outputfile = open("log.csv","a")
+  outputfile.write(headers)
+
+print headers
 
 try:
   while True:
-    data = time.strftime("%x %X,", time.gmtime()) + " " + ", ".join(map(str,bme680read.getenviromentaldata() + tsl2561read.getlightdata()))
+    data = time.strftime("%X %x,", time.gmtime()) + " " + ", ".join(map(str,bme680read.getenviromentaldata() + tsl2561read.getlightdata()))
     print data
 
     data = data + "\n"
     outputfile.write(data)
 
-    time.sleep(2)
+    time.sleep(loggingperiod)
  
 except KeyboardInterrupt:
   pass
