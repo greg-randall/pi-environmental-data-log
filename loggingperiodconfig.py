fdaf441eebf039923fe2dc3de5fa8e23db777876
Greg Randall <gregrr@gmail.com>
2018-05-13 20:21:00 -0500

    changed logging period

--- loggingperiodconfig.py
+++ loggingperiodconfig.py
@@ -1,8 +1,8 @@
 #logging period in seconds
 #decimal seconds ok; not sure on lower limit.
 #0.5 seconds seems to work fine
-loggingperiod = 0.2
+loggingperiod = 2
 
 
 #time to wait before uploading a csv in minutes
-uploadperiod = 0.1
+uploadperiod = 90
