249a5d65dd58932239e9320b191b328649f1604e
Greg Randall <gregrr@gmail.com>
2018-05-17 12:06:35 -0500

    added a bunch of comments to the the ftp uplad file

--- ftp-upload-data.py
+++ ftp-upload-data.py
@@ -1,40 +1,40 @@
-import time
+#import time
 from datetime import datetime
 import os
 from ftplib import FTP
 from ftpconfig import * #credentials for ftp. done this way to keep them from getting added to git
 
-while True:
-  filestoupload = os.listdir('data/upload')
-  if len(filestoupload)>0:
-    print "Found " + str(len(filestoupload)) + " file(s) to upload: "
-    try:
+while True: #loop forever
+  filestoupload = os.listdir('data/upload') #get a list of the files you need to upload
+  if len(filestoupload)>0: #check if there are files to upload
+    print "Found " + str(len(filestoupload)) + " file(s) to upload: " #list the file count to upload
+    try: #make sure the ftp stuff actually works, if it doesn't we'll throw an error message
+      #initate ftp connection using the the information in the ftp config file
       ftp = FTP()
       ftp.connect(SERVER, PORT)
       ftp.login(USER, PASS)
 
- 
+      #get list of files on the ftp server to see if the folder we need to upload to exisits
       filelist = []
       ftp.retrlines('LIST', filelist.append)
-
       found = False
-      for f in filelist:
+      for f in filelist: #look through file list to find if the folder is there
         filename = f.split()[-1]
-        if filename == 'pi-env-data':
+        if filename == 'pi-env-data': #if we find the folder change directories and break the loop
           found = True
           ftp.cwd('pi-env-data')
           break
-      if not found:
+      if not found: #if we didn't find the folder create the folder and then go into that directory
         ftp.mkd('pi-env-data')
         ftp.cwd('pi-env-data')
-    
-      for f in filestoupload:
-        print f + "\n" 
-        ftp.storbinary('STOR ' + f, open("data/upload/" + f))
-        os.rename("data/upload/" + f,"data/uploaded/" + f)
-      ftp.close()
+
+      for f in filestoupload: #loop through the files we need to upload
+        print f + "\n" #list the file we're currently uploading
+        ftp.storbinary('STOR ' + f, open("data/upload/" + f)) #upload the file
+        os.rename("data/upload/" + f,"data/uploaded/" + f) #move the file we just uploaded into the uploaded directory
+      ftp.close() #after all of the uploads close the ftp connection
     except:
-      print SERVER + " not available. Will retry shortly."
-  
-  print "Waiting 30 seconds. Current time: " + datetime.now().strftime("%H:%M:%S.%f")
-  time.sleep(30)
+      print "Could not access " + SERVER + ". Will retry shortly." #if we can't get to the server then list that it failed
+
+  print "Waiting 30 seconds. Current time: " + datetime.now().strftime("%H:%M:%S.%f") #wait statment before checking for new files to upload, time is there to help debugging
+  time.sleep(30) #wait until we check the folders again
