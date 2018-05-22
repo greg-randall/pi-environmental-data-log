4eaedd2c26f4b21e71efdd3c53003dff99339f3b
greg-randall <gregrr@gmail.com>
2018-05-21 23:05:59 -0500

    set the default logging rate to twice a second, and upload the log file every hour

--- loggingperiodconfig.py
+++ loggingperiodconfig.py
@@ -1,8 +1,8 @@
 #logging period in seconds
 #decimal seconds ok; not sure on lower limit.
 #0.5 seconds seems to work fine
-loggingperiod = 2
+loggingperiod = 0.5
 
 
 #time to wait before uploading a csv in minutes
-uploadperiod = 90
+uploadperiod = 60
